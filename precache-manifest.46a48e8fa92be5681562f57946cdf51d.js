68b1078209b3068224307e3c46c7d71a8bd4dd24
Yunsdev <yunsdev>
2020-05-20 19:39:11 +0900

    Updates

---
+++ precache-manifest.46a48e8fa92be5681562f57946cdf51d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "71fc31e90af77139eb25",
+    "url": "/expo-Dashboard/static/js/app.66be0359.chunk.js"
+  },
+  {
+    "revision": "77ba3c3fb0dfcb6385a8",
+    "url": "/expo-Dashboard/static/js/runtime~app.5b516df9.js"
+  },
+  {
+    "revision": "d9aaafde5f24ed69f81a",
+    "url": "/expo-Dashboard/static/js/2.7d75fab0.chunk.js"
+  },
+  {
+    "revision": "f0274289bf23fb548898f00651b36d6b",
+    "url": "/expo-Dashboard/static/media/logo.f0274289.png"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/expo-Dashboard/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/expo-Dashboard/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/expo-Dashboard/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/expo-Dashboard/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/expo-Dashboard/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/expo-Dashboard/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/expo-Dashboard/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/expo-Dashboard/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/expo-Dashboard/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/expo-Dashboard/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/expo-Dashboard/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/expo-Dashboard/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-Dashboard/expo-service-worker.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/expo-Dashboard/serve.json"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/expo-Dashboard/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/expo-Dashboard/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/expo-Dashboard/favicon.ico"
+  },
+  {
+    "revision": "4ea12eb5e73f365ee89ffeffa4779651",
+    "url": "/expo-Dashboard/index.html"
+  },
+  {
+    "revision": "fb3992f60c858eb2b3208d4a1f773f93",
+    "url": "/expo-Dashboard/manifest.json"
+  },
+  {
+    "revision": "9797bec15b5a0bb73b425deeac8aa661",
+    "url": "/expo-Dashboard/register-service-worker.js"
+  }
+];
